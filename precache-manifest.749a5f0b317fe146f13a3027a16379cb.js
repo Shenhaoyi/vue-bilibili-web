3b35520ece4cea47aa4465be23c0c83773fd4276
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-09-05 22:54:57 +0800

    init

---
+++ precache-manifest.749a5f0b317fe146f13a3027a16379cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f0b13f7e0cd373ff10",
+    "url": "/vue-bilibili-web/css/app.55d8b5aa.css"
+  },
+  {
+    "revision": "e341a5a2610040136c20",
+    "url": "/vue-bilibili-web/css/chunk-vendors.74b207a1.css"
+  },
+  {
+    "revision": "1d9a208e64479faac4204c50cf7997e4",
+    "url": "/vue-bilibili-web/img/bannerTop_new.1d9a208e.png"
+  },
+  {
+    "revision": "9ef3f73d28c3f06efa1407a62b36df4a",
+    "url": "/vue-bilibili-web/index.html"
+  },
+  {
+    "revision": "85f0b13f7e0cd373ff10",
+    "url": "/vue-bilibili-web/js/app.a995b3e0.js"
+  },
+  {
+    "revision": "e341a5a2610040136c20",
+    "url": "/vue-bilibili-web/js/chunk-vendors.40897f06.js"
+  },
+  {
+    "revision": "3787153eca851027b2a90490211db06d",
+    "url": "/vue-bilibili-web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-bilibili-web/robots.txt"
+  }
+]);
